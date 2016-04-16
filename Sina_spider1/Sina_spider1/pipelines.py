292eed5621df23e2f1913c6c02ff95eada5b7b07
Zhe Li <zhe.li@jetshop.se>
2016-04-16 18:04:32 +0200

    只使用抓粉丝功能

--- Sina_spider1/Sina_spider1/pipelines.py
+++ Sina_spider1/Sina_spider1/pipelines.py
@@ -7,39 +7,12 @@ class MongoDBPipleline(object):
     def __init__(self):
         clinet = pymongo.MongoClient("localhost", 27017)
         db = clinet["Sina"]
-        self.Information = db["Information"]
-        self.Tweets = db["Tweets"]
-        self.Follows = db["Follows"]
-        self.Fans = db["Fans"]
+        self.fan_list = db["fan_list"]
 
     def process_item(self, item, spider):
-        """ 判断item的类型，并作相应的处理，再入数据库 """
-        if isinstance(item, InformationItem):
-            try:
-                self.Information.insert(dict(item))
-            except Exception:
-                pass
-        elif isinstance(item, TweetsItem):
-            try:
-                self.Tweets.insert(dict(item))
-            except Exception:
-                pass
-        elif isinstance(item, FollowsItem):
-            followsItems = dict(item)
-            follows = followsItems.pop("follows")
-            for i in range(len(follows)):
-                followsItems[str(i + 1)] = follows[i]
-            try:
-                self.Follows.insert(followsItems)
-            except Exception:
-                pass
-        elif isinstance(item, FansItem):
-            fansItems = dict(item)
-            fans = fansItems.pop("fans")
-            for i in range(len(fans)):
-                fansItems[str(i + 1)] = fans[i]
-            try:
-                self.Fans.insert(fansItems)
-            except Exception:
-                pass
+        fans = [dict(_id=id, name=name, url=url) for id, name, url in item['fans']]
+        self.fan_list.update(
+            {'_id': item['_id'], 'name': item['name']},
+            {'$addToSet':{'fans':{'$each': fans }}},
+            upsert=True)
         return item
